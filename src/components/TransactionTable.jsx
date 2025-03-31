ecf123f460ae67031503676e1f3e2bee130b735a
maggy <perezmag5137@gmail.com>
2025-03-31 17:15:52 -0400

    added coloring to the table to help recognize which values are positive or negative

--- src/components/TransactionTable.jsx
+++ src/components/TransactionTable.jsx
@@ -36,8 +36,15 @@ function TransactionTable() {
                     {transData.map((val, key) => {
                         return (
                             <tr key={key}>
+                                {/* Displaying amount in green if amount > 0, otherwise it's red */}
                                 <td>{val.Account}</td>
-                                <td>${val.Amount.toFixed(2)}</td>
+                                <td>
+                                    {val.Amount.toFixed(2) > 0 ? (
+                                        <span style={{color: "green"}}>${val.Amount.toFixed(2)}</span>
+                                    ) : (
+                                        <span style={{color: "red"}}>${val.Amount.toFixed(2)}</span>
+                                    )}
+                                </td>
                                 <td>{val.TransactionType}</td>
                                 <td>${val.AvailableBalance.toFixed(2)}</td>
                                 <td>{val.Date}</td>
